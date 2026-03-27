f02b68bdbdf8ff3feb496496476b082c5c8dd2a6
Jenisha <jenishereyl@gmail.com>
2026-03-27 10:58:38 +0530

    feat: add hyperparameter tuning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -43,11 +43,59 @@ class ModelTrainer:
                 "Linear Regression" : LinearRegression(),
                 "K-Neighbors Regressor" : KNeighborsRegressor(),
                 "XGBRegressor" : XGBRegressor(),
-                "CatBoost": CatBoostRegressor(verbose=0),
-                "AdaBoost" : AdaBoostRegressor()
+                "CatBoosting Regressor": CatBoostRegressor(verbose=0),
+                "AdaBoost Regressor" : AdaBoostRegressor()
             }
 
-            model_report : dict = evaluate_models(X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test, models=models)
+            params = {
+
+                "Linear Regression": {},
+                "Decision Tree": {
+                    'max_depth': [None, 5, 10, 20],
+                    'min_samples_split': [2, 5, 10],
+                    'min_samples_leaf': [1, 2, 4]
+                },
+                "Random Forest": {
+                    'n_estimators': [50, 100, 200],
+                    'max_depth': [None, 10, 20],
+                    'min_samples_split': [2, 5],
+                    'min_samples_leaf': [1, 2],
+                    'max_features': ['sqrt', 'log2']
+                },
+                "Gradient Boosting": {
+                    'learning_rate': [0.1, 0.05, 0.01],
+                    'n_estimators': [50, 100, 200],
+                    'subsample': [0.7, 0.8, 0.9],
+                    'max_depth': [3, 5, 7]
+                },
+                "K-Neighbors Regressor": {   
+                    'n_neighbors': [3, 5, 7, 9],
+                    'weights': ['uniform', 'distance'],
+                    'algorithm': ['auto', 'ball_tree', 'kd_tree'],
+                    'p': [1, 2]  
+                },
+                "XGBRegressor": {
+                    'learning_rate': [0.1, 0.05, 0.01],
+                    'n_estimators': [50, 100, 200],
+                    'max_depth': [3, 5, 7],
+                    'subsample': [0.7, 0.8],
+                    'colsample_bytree': [0.7, 0.8]
+                },
+                "CatBoosting Regressor": {
+                    'depth': [6, 8, 10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [50, 100, 200],
+                    'l2_leaf_reg': [1, 3, 5]
+                },
+                "AdaBoost Regressor": {
+                    'learning_rate': [0.1, 0.05, 0.01],
+                    'n_estimators': [50, 100, 200],
+                    'loss': ['linear', 'square']
+                }
+            }
+
+
+            model_report : dict = evaluate_models(X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test, models=models, param=params)
 
             best_model_score  = max(sorted(model_report.values()))
 
