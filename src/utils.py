2bc54161187341d11f890b726c4e1ef315bba83e
Jenisha <jenishereyl@gmail.com>
2026-03-27 19:27:21 +0530

    feat: add Deployment configuration

--- src/utils.py
+++ src/utils.py
@@ -33,7 +33,6 @@ def evaluate_models(X_train, y_train, X_test, y_test, models, param):
             model.set_params(**gs.best_params_)
             model.fit(X_train,y_train)
 
-            # model.fit(X_train, y_train)
             y_train_pred = model.predict(X_train)
             y_test_pred = model.predict(X_test)
 
@@ -43,5 +42,13 @@ def evaluate_models(X_train, y_train, X_test, y_test, models, param):
             report[list(models.keys())[i]] = test_model_score
 
         return report
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def load_object(file_path):
+    try: 
+        with open(file_path, "rb") as file_obj:
+            return dill.load(file_obj)
+        
     except Exception as e:
         raise CustomException(e, sys)
